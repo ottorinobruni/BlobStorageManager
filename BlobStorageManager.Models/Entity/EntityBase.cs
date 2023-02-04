bedb8fc7970bf13cf7411e8762c551ab98f0e4a8
Ottorino Bruni <obruni@arthrex.com>
2023-02-04 10:41:48 +0100

    Add Entity

---
+++ BlobStorageManager.Models/Entity/EntityBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlobStorageManager.Models.Entity
+{
+    public class EntityBase
+    {
+        public Guid Id { get; set; }
+
+        public DateTime? CreatedOn { get; set; }
+    }
+}
---
+++ BlobStorageManager.Models/Entity/FileStorage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlobStorageManager.Models.Entity
+{
+    public class FileStorage : EntityBase
+    {
+        public string? FileName { get; set; }
+
+        public string? Uri { get; set; }
+
+        public string? ContentType { get; set; }
+
+        public DateTime? UploadDateTime { get; set; }
+    }
+}

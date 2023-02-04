98d3a0c8b2cc77ff142805fbf376d22f45961b24
Ottorino Bruni <obruni@arthrex.com>
2023-02-04 11:52:33 +0100

    Add FileStorageController

---
+++ BlobStorageManager.Api/Controllers/FileStorageController.cs
@@ -0,0 +1,26 @@
+﻿using BlobStorageManager.Api.Models;
+using BlobStorageManager.Models.Entity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlobStorageManager.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileStorageController : ControllerBase
+    {
+        private readonly ILogger<FileStorageController> logger;
+        private readonly IFileStorageRepository fileStorageRepository;
+
+        public FileStorageController(ILogger<FileStorageController> logger, IFileStorageRepository fileStorageRepository)
+        {
+            this.logger = logger;
+            this.fileStorageRepository = fileStorageRepository;
+        }
+
+        [HttpGet("files")]
+        public async Task<ActionResult<List<FileStorage>>> GetFiles()
+        {
+            return await fileStorageRepository.GetFiles();
+        }
+    }
+}
